9db2c7023ada592766e8374429e1ceba1876264d
Denis Craig <dcraig@thousandeyes.com>
2018-11-26 16:04:19 +0000

    Use multiple modules for each region instead

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-variable "ds-regions" {
-    type = "list"
-    default = ["us-east-1", "eu-west-2", "ap-southeast-2"]
-}
-
 terraform {
     backend "s3" {
         bucket      = "sourceforts-infrastructure-state"
@@ -15,8 +10,17 @@ module "updater" {
     source = "modules/updater"
 }
 
-module "dedicated-server-cluster" {
+module "us-east-1" {
     source = "modules/dedicated-server-cluster"
-    count = "${length(var.ds-regions)}"
-    region = "${var.ds-regions[count]}"
+    region = "us-east-1"
+}
+
+module "eu-west-2" {
+    source = "modules/dedicated-server-cluster"
+    region = "eu-west-2"
+}
+
+module "ap-southeast-2" {
+    source = "modules/dedicated-server-cluster"
+    region = "ap-southeast-2"
 }
