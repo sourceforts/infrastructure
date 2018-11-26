e60b86382ee45e86dd3c816ff277fbcfcf4d8995
Denis Craig <admin@deniscraig.com>
2018-11-26 21:44:31 +0000

    We always want to get the latest AMI

--- modules/aws/ecs/instances/main.tf
+++ modules/aws/ecs/instances/main.tf
@@ -100,7 +100,6 @@ resource "aws_security_group_rule" "outbound_internet_access_other_udp" {
 }
 
 data "aws_ami" "ecs_ami" {
-  count       = "${var.lookup_latest_ami ? 1 : 0}"
   most_recent = true
 
   filter {
@@ -126,7 +125,7 @@ data "aws_ami" "ecs_ami" {
 
 resource "aws_launch_configuration" "launch_configuration" {
     name_prefix             = "${var.name}-${var.cluster_name}-${var.instance_group}-"
-    image_id                = "${var.lookup_latest_ami ? join("", data.aws_ami.ecs_ami.*.image_id) : join("", data.aws_ami.user_ami.*.image_id)}"
+    image_id                = "${join("", data.aws_ami.ecs_ami.*.image_id)}"
     instance_type           = "${var.instance_type}"
     user_data               = "${data.template_file.user_data.rendered}"
     iam_instance_profile    = "${var.iam_instance_profile_id}"
