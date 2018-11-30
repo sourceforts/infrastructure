f4e873b601976dad728caa7cbae801b26708cf77
Denis Craig <dcraig@thousandeyes.com>
2018-11-30 21:39:04 +0000

    Add NS records to root domain

--- modules/aws/route53/subdomain/main.tf
+++ modules/aws/route53/subdomain/main.tf
@@ -6,17 +6,31 @@ resource "aws_route53_zone" "hosted_zone" {
 }
 
 resource "aws_route53_record" "nameservers" {
-  zone_id = "${aws_route53_zone.hosted_zone.zone_id}"
-  name    = "${var.name}.${var.root_domain}"
-  type    = "NS"
-  ttl     = "30"
+    zone_id = "${aws_route53_zone.hosted_zone.zone_id}"
+    name    = "${var.name}.${var.root_domain}"
+    type    = "NS"
+    ttl     = "30"
 
-  records = [
+    records = [
     "${aws_route53_zone.hosted_zone.name_servers.0}",
     "${aws_route53_zone.hosted_zone.name_servers.1}",
     "${aws_route53_zone.hosted_zone.name_servers.2}",
     "${aws_route53_zone.hosted_zone.name_servers.3}",
-  ]
+    ]
+}
+
+resource "aws_route_53_record" "root_nameservers" {
+    zone_id = "${var.root_zone_id}"
+    name    = "${var.name}.${var.root_domain}"
+    type    = "NS"
+    ttl     = "30"
+
+    records = [
+        "${aws_route53_zone.hosted_zone.name_servers.0}",
+        "${aws_route53_zone.hosted_zone.name_servers.1}",
+        "${aws_route53_zone.hosted_zone.name_servers.2}",
+        "${aws_route53_zone.hosted_zone.name_servers.3}",
+    ]
 }
 
 resource "aws_route53_record" "eip" {
