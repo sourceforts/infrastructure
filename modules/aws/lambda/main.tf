5c665ae1294db8eec1286c1776a1a9b0571f270f
Denis Craig <dcraig@thousandeyes.com>
2018-12-01 13:21:16 +0000

    Add IAM policy for logging to cloudwatch from lambda

--- modules/aws/lambda/main.tf
+++ modules/aws/lambda/main.tf
@@ -17,6 +17,34 @@ resource "aws_iam_role" "function_iam_role" {
 }
 EOF
 }
+
+resource "aws_iam_policy" "function_logging" {
+  name = "lambda_logging"
+  path = "/"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "function_logging_attachment" {
+  role = "${aws_iam_role.function_iam_role.name}"
+  policy_arn = "${aws_iam_policy.function_logging.arn}"
+}
+
 resource "aws_lambda_function" "function" {
     function_name   = "${var.name}"
     s3_bucket       = "${var.bucket_name}"
