32afd2b896eb8e82bd991a38d9efede1c2a94b5d
Denis Craig <dcraig@thousandeyes.com>
2018-12-01 02:27:57 +0000

    Define missing handler

--- modules/aws/lambda/main.tf
+++ modules/aws/lambda/main.tf
@@ -21,6 +21,7 @@ resource "aws_lambda_function" "function" {
     function_name   = "${var.name}"
     s3_bucket       = "${var.bucket_name}"
     s3_key          = "${var.name}.zip"
+    handler         = "exports.handler"
     runtime         = "nodejs8.10"
     role            = "${aws_iam_role.function_iam_role.arn}"
     memory_size     = 1024
