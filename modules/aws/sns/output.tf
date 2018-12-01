9ea1efe182aedc3fb236a46ff570fb24338f3e74
Denis Craig <dcraig@thousandeyes.com>
2018-12-01 23:12:25 +0000

    try topic id to see the error message

--- modules/aws/sns/output.tf
+++ modules/aws/sns/output.tf
@@ -1,3 +1,3 @@
 output "topic_arn" {
-    value = "${aws_sns_topic.topic.arn}"
+    value = "${aws_sns_topic.topic.id}"
 }
--- modules/platform/server-notifier/main.tf
+++ modules/platform/server-notifier/main.tf
@@ -16,15 +16,15 @@ module "handler" {
 }
 
 resource "aws_lambda_permission" "handler_permission" {
-  statement_id  = "AllowExecutionFromSNS"
-  action        = "lambda:InvokeFunction"
-  function_name = "${local.function_name}"
-  principal     = "sns.amazonaws.com"
-  source_arn    = "${module.topic.topic_arn}"
+    statement_id  = "AllowExecutionFromSNS"
+    action        = "lambda:InvokeFunction"
+    function_name = "${local.function_name}"
+    principal     = "sns.amazonaws.com"
+    source_arn    = "${module.topic.topic_arn}"
 }
 
 resource "aws_sns_topic_subscription" "handler_subscription" {
-  topic_arn = "${module.topic.topic_arn}"
-  protocol  = "lambda"
-  endpoint  = "${module.handler.lambda_arn}"
+    topic_arn = "${module.topic.topic_arn}"
+    protocol  = "lambda"
+    endpoint  = "${module.handler.lambda_arn}"
 }
