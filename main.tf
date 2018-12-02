3909db4290a2c4070f265f7b5a25a602dc5a8a50
Denis Craig <dcraig@thousandeyes.com>
2018-12-02 19:40:54 +0000

    Update VPC peering

--- main.tf
+++ main.tf
@@ -50,20 +50,32 @@ module "ap-southeast-2" {
 
 resource "aws_vpc_peering_connection" "eu_us_peer" {
     provider    = "aws.eu-west-2"
-    peer_vpc_id = "${module.eu-west-2.vpc_id}"
-    vpc_id      = "${module.us-east-1.vpc_id}"
+    region      = "eu-west-2"
+
+    vpc_id      = "${module.eu-west-2.vpc_id}"
+    peer_vpc_id = "${module.us-east-1.vpc_id}"
+    peer_region = "us-east-1"
+    auto_accept = true
 }
 
 resource "aws_vpc_peering_connection" "eu_ap_peer" {
     provider    = "aws.eu-west-2"
-    peer_vpc_id = "${module.eu-west-2.vpc_id}"
-    vpc_id      = "${module.ap-southeast-2.vpc_id}"
+    region      = "eu-west-2"
+
+    vpc_id      = "${module.eu-west-2.vpc_id}"
+    peer_vpc_id = "${module.ap-southeast-2.vpc_id}"
+    peer_region = "ap-southeast-2"
+    auto_accept = true
 }
 
 resource "aws_vpc_peering_connection" "us_ap_peer" {
-    provider    = "aws.eu-west-2"
-    peer_vpc_id = "${module.us-east-1.vpc_id}"
-    vpc_id      = "${module.ap-southeast-2.vpc_id}"
+    provider    = "aws.us-east-1"
+    region      = "us-east-1"
+
+    vpc_id      = "${module.us-east-1.vpc_id}"
+    peer_vpc_id = "${module.ap-southeast-2.vpc_id}"
+    peer_region = "ap-southeast-2"
+    auto_accept = true
 }
 
 module "updater-platform" {
