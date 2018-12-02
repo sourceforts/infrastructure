a84f1156e1d38369db36966a777e44c902f002f0
Denis Craig <dcraig@thousandeyes.com>
2018-12-02 23:41:47 +0000

    Accept VPC peer connection from other side

--- main.tf
+++ main.tf
@@ -71,6 +71,12 @@ resource "aws_vpc_peering_connection" "eu_us_peer" {
     peer_region = "us-east-1"
 }
 
+resource "aws_vpc_peering_connection_accepter" "eu_us_peer_accept" {
+    provider                  = "aws.us-east-1"
+    vpc_peering_connection_id = "${aws_vpc_peering_connection.eu_us_peer.id}"
+    auto_accept               = true
+}
+
 resource "aws_vpc_peering_connection" "eu_ap_peer" {
     provider    = "aws.eu-west-2"
 
@@ -79,12 +85,24 @@ resource "aws_vpc_peering_connection" "eu_ap_peer" {
     peer_region = "ap-southeast-2"
 }
 
+resource "aws_vpc_peering_connection_accepter" "eu_ap_peer_accept" {
+    provider                  = "aws.ap-southeast-2"
+    vpc_peering_connection_id = "${aws_vpc_peering_connection.eu_ap_peer.id}"
+    auto_accept               = true
+}
+
 resource "aws_vpc_peering_connection" "us_ap_peer" {
     provider    = "aws.us-east-1"
 
     vpc_id      = "${module.us-east-1.vpc_id}"
     peer_vpc_id = "${module.ap-southeast-2.vpc_id}"
     peer_region = "ap-southeast-2"
+}
+
+resource "aws_vpc_peering_connection_accepter" "us_ap_peer_accept" {
+    provider                  = "aws.ap-southeast-2"
+    vpc_peering_connection_id = "${aws_vpc_peering_connection.us_ap_peer.id}"
+    auto_accept               = true
 }
 
 module "updater-platform" {
