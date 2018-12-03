6aa2d657aa610f5b07d456e014ede298a2e5b460
Denis Craig <dcraig@thousandeyes.com>
2018-12-03 22:33:08 +0000

    Add port vars to cluster

--- modules/aws/ecs/cluster/vars.tf
+++ modules/aws/ecs/cluster/vars.tf
@@ -47,3 +47,34 @@ variable "consul_tag_key" {
 variable "consul_tag_value" {
     default = "sf-disc"
 }
+
+variable "server_rpc_port" {
+  description = "The port used by servers to handle incoming requests from other agents."
+  default     = 8300
+}
+
+variable "cli_rpc_port" {
+  description = "The port used by all agents to handle RPC from the CLI."
+  default     = 8400
+}
+
+variable "serf_lan_port" {
+  description = "The port used to handle gossip in the LAN. Required by all agents."
+  default     = 8301
+}
+
+variable "serf_wan_port" {
+  description = "The port used by servers to gossip over the WAN to other servers."
+  default     = 8302
+}
+
+variable "http_api_port" {
+  description = "The port used by clients to talk to the HTTP API"
+  default     = 8500
+}
+
+variable "dns_port" {
+  description = "The port used to resolve DNS queries."
+  default     = 8600
+}
+
